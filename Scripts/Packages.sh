e8666ff44a9aff029a555120758fc3e4b0d0cd6f
Mr.Zhou <zero187735@163.com>
2026-06-12 23:44:23 +0800

    Add netbird package to Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -62,6 +62,7 @@ UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+UPDATE_PACKAGE "netbird" "netbirdio/openwrt-netbird" "main" "pkg"
 
 #UPDATE_PACKAGE "athena-led" "unraveloop/JDC-AX6600-Athena-LED-Controller" "main"
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
